1ba84acaa3135a3e10eb448b228f6deebf32d9f9
Camilo Higuita <chiguitar@unal.edu.co>
2022-08-23 15:07:36 -0500

    initial working version

---
+++ libs/power/src/code/caskpowerplugin.cpp
@@ -0,0 +1,17 @@
+#include "caskpowerplugin.h"
+
+
+#include <QQmlEngine>
+
+#include "brightnesscontrol.h"
+#include "batteryinfo.h"
+#include "powerprofile.h"
+
+void PowerPlugin::registerTypes(const char *uri)
+{
+    qmlRegisterType<BrightnessControl>(uri, 1, 0, "BrightnessControl");
+    qmlRegisterType<BatteryInfo>(uri, 1, 0, "BatteryInfo");
+    qmlRegisterUncreatableType<Battery>(uri, 1, 0, "Battery", "To be reference from the model or controller.");
+    qmlRegisterType<PowerProfile>(uri, 1, 0, "PowerProfile");
+
+}
