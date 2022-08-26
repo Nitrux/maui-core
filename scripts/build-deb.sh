ae52934fc393aec297f15a51b128b8ee1bd11f64
Uri Herrera <hello@uriherrera.com>
2022-08-26 15:13:41 -0500

    update stuff

---
+++ scripts/build-deb.sh
@@ -0,0 +1,81 @@
+#! /bin/bash
+
+set -x
+
+### Update sources
+
+wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.neon.user
+
+wget -qO /etc/apt/sources.list.d/nitrux-main-compat-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.nitrux
+
+wget -qO /etc/apt/sources.list.d/nitrux-testing-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.nitrux.testing
+
+DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
+	55751E5D > /dev/null
+
+curl -L https://packagecloud.io/nitrux/repo/gpgkey | apt-key add -;
+curl -L https://packagecloud.io/nitrux/compat/gpgkey | apt-key add -;
+curl -L https://packagecloud.io/nitrux/testing/gpgkey | apt-key add -;
+
+DEBIAN_FRONTEND=noninteractive apt -qq update
+
+### Install Package Build Dependencies #2
+
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
+	mauikit-git \
+	mauikit-filebrowsing-git
+
+### Download Source
+
+git clone --depth 1 --branch $MAUICORE_BRANCH https://github.com/Nitrux/maui-core.git
+
+
+### Compile Source
+
+mkdir -p build && cd build
+
+cmake \
+	-DCMAKE_INSTALL_PREFIX=/usr \
+	-DENABLE_BSYMBOLICFUNCTIONS=OFF \
+	-DQUICK_COMPILER=ON \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_SYSCONFDIR=/etc \
+	-DCMAKE_INSTALL_LOCALSTATEDIR=/var \
+	-DCMAKE_EXPORT_NO_PACKAGE_REGISTRY=ON \
+	-DCMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY=ON \
+	-DCMAKE_INSTALL_RUNSTATEDIR=/run "-GUnix Makefiles" \
+	-DCMAKE_VERBOSE_MAKEFILE=ON \
+	-DCMAKE_INSTALL_LIBDIR=lib/x86_64-linux-gnu ../maui-core/
+
+make -j$(nproc)
+
+### Run checkinstall and Build Debian Package
+
+>> description-pak printf "%s\n" \
+	'MauiKit core libraries to manage the DE.' \
+	'' \
+	'Core libraries to manage the DE.' \
+	'' \
+	'Shared between Maui Settings and Cask.' \
+	'' \
+	''
+
+checkinstall -D -y \
+	--install=no \
+	--fstrans=yes \
+	--pkgname=maui-core-git \
+	--pkgversion=$PACKAGE_VERSION \
+	--pkgarch=amd64 \
+	--pkgrelease="1" \
+	--pkglicense=LGPL-3 \
+	--pkggroup=utils \
+	--pkgsource=maui-core \
+	--pakdir=. \
+	--maintainer=uri_herrera@nxos.org \
+	--provides=maui-core \
+	--requires=libc6,libgcc-s1,libstdc++6 \
+	--nodoc \
+	--strip=no \
+	--stripso=yes \
+	--reset-uids=yes \
+	--deldesc=yes
---
+++ scripts/install-build-deps.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+    APT_COMMAND="sudo apt-get"
+else
+    APT_COMMAND="apt-get"
+fi
+
+$APT_COMMAND update -q
+$APT_COMMAND install -qy --no-install-recommends \
+    appstream \
+    automake \
+    autotools-dev \
+    build-essential \
+    checkinstall \
+    cmake \
+    curl \
+    devscripts \
+    equivs \
+    extra-cmake-modules \
+    gettext \
+    git \
+    gnupg2 \
+    libkf5config-dev \
+    libkf5coreaddons-dev \
+    libkf5i18n-dev \
+    libkf5kio-dev \
+    libkf5notifications-dev \
+    libwayland-dev \
+    lintian \
+    qtbase5-dev \
+    qtdeclarative5-dev \
+    qtquickcontrols2-5-dev \
+    qtwayland5 \
+    qtwayland5-dev-tools \
+    qtwayland5-private-dev
